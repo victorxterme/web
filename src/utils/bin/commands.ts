37f2361033030fb5a4b455baea3c9a8ce87edd87
victorxterme <149105449+victorxterme@users.noreply.github.com>
2024-02-07 23:02:59 -0500

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -17,6 +17,8 @@ export const help = async (args: string[]): Promise<string> => {
   return `Hi, here are all the commands:
   - about
   - github
+  - projects
+  - classes
   - linkedin
   - email
   - resume
@@ -29,7 +31,7 @@ export const help = async (args: string[]): Promise<string> => {
 export const about = async (args: string[]): Promise<string> => {
   return `Hello, my name is ${config.name}. 
 I am a student at Waterloo University currently in my first year, majoring in Computer Science.
-Some of my hobbies are coding, drawing going to the gym, and rock climbing. I have some projects listed on github.
+Some of my hobbies are coding, drawing going to the gym, and rock climbing.
 Some skills I have are time-managment, problem solving, communication and organization. To learn
 more about me, type 'help'.`;
 };
@@ -39,6 +41,57 @@ export const resume = async (args: string[]): Promise<string> => {
   return 'Opening resume...';
 };
 
+// projects 
+export const projects = async (args: string[]): Promise<string> => {
+  return `Here are some of my projects: 
+  - Sudoku Solver
+  - WARG automated landing detection
+  For more details, enter the first word of each project.
+  - API backend`;
+
+};
+
+export const Sudoku = async (args: string[]): Promise<string> => {
+  return `I built a sudoku solver in python exclusively on my own. I required me to design its UI, create a 
+  valid board game every time it's generated, choose valid starting cells, and implement a backtracking algorithm
+  to solve the game. I learned a lot about how to combine the algorithm to solve the sudoku with 
+  what I was displaying, and I also learned ways to organize my code better.`;
+};
+export const sudoku = async (args: string[]): Promise<string> => {
+  return `I built a sudoku solver in python exclusively on my own. I required me to design its UI, create a 
+  valid board game every time it's generated, choose valid starting cells, and implement a backtracking algorithm
+  to solve the game. I learned a lot about how the internal process of my code solving the sudoku interacted with 
+  what I was displaying, and I also learned how to organize my code better.`;
+};
+export const WARG = async (args: string[]): Promise<string> => {
+  return `I joined the Waterloo Aerial Robotics Group (Warg) in early january, and have started learning how to use
+  ultralytics (python module) for tracking and detecting real objects. I learned how to implement a drone detecting a landing pad, 
+  how to make a drone go from one point to another, and land in a simulation.`;
+};
+export const warg = async (args: string[]): Promise<string> => {
+  return `I joined the Waterloo Aerial Robotics Group (Warg) in early january, and have started learning how to use
+  ultralytics (python module) for tracking and detecting real objects. I learned how to implement a drone detecting a landing pad, 
+  how to make a drone go from one point to another, and land in a simulation.`;
+};
+export const API = async (args: string[]): Promise<string> => {
+  return `Learned how to use Node.JS to create a backend for my websites, and used MongoDB for the database. This allowed 
+  my to develop my javascript skills. I plan to create a website that reccomends a movie based on user inputs using a web scraper.`;
+};
+export const api = async (args: string[]): Promise<string> => {
+  return `Learned how to use Node.JS to create a backend for my websites, and used MongoDB for the database. This allowed 
+  my to develop my javascript skills. I plan to create a website that reccomends a movie based on user inputs using a web scraper.`;
+};
+
+
+export const classes = async (args: string[]): Promise<string> => {
+  return `These are the classes I'm currently taking:
+  - Statistics (Analytical methods)
+  - Linear Algebra
+  - CS (Elementary Algorithm Design and Data Abstraction)
+  - CS Lab (Unix)
+  - Commerce 
+  - Communication Class`;
+};
 
 
 // Contact
