a41e2484012035d5e0139f1713214610c1920d69
victorxterme <149105449+victorxterme@users.noreply.github.com>
2024-02-07 23:20:54 -0500

    Update api_commands.ts

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -5,15 +5,6 @@ import { getQuote } from '../api';
 import { getReadme } from '../api';
 import { getWeather } from '../api';
 
-export const projects = async (args: string[]): Promise<string> => {
-  const projects = await getProjects();
-  return projects
-    .map(
-      (repo) =>
-        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
-    )
-    .join('\n');
-};
 
 export const quote = async (args: string[]): Promise<string> => {
   const data = await getQuote();
